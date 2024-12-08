57f4d62a0bba8c45600a37f887a7941a11e45566
HrishikeshPK <hrishilovjith@gmail.com>
2024-12-08 20:12:40 +0530

    requests page

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -12,7 +12,7 @@ const Connections = () => {
     const fetchConnections = async ()=> {
         try {
             const res = await axios.get(BASE_URL+"/user/connections",{ withCredentials: true})
-            // console.log(res.data.data)
+            console.log(res.data.data)
             dispatch(addConnections(res.data.data))
         } catch (err) {
             console.log(err)
@@ -31,22 +31,33 @@ const Connections = () => {
         <div className='flex justify-center my-10'>
             <h1 className='font-bold text-3xl'>Connections</h1> 
         </div>
-       <div className='flex justify-center'>
+
+        {
+          connections.map((connection)=> {
+            const {_id, firstName, lastName, photoUrl, age, gender, about } = connection
+
+            return (
+              <div key={_id} className='flex justify-center'>
        <div className="card card-side bg-base-300 shadow-xl w-1/2 ">
   <figure>
     <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      src={photoUrl}
       alt="Movie" />
   </figure>
   <div className="card-body">
-    <h2 className="card-title">Name</h2>
-    <p>About</p>
+    <h2 className="card-title">{firstName+" "+lastName}</h2>
+    { age && gender && <p>{age + ", " + gender}</p> }
+    <p>{about}</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Message</button>
     </div>
   </div>
 </div>
        </div>
+            )
+          })
+        }
+       
     </div>    
   )
 }
---
+++ src/components/Requests.jsx
@@ -0,0 +1,127 @@
+// import axios from "axios"
+// import { BASE_URL } from "../utils/constants"
+// import { useDispatch, useSelector } from "react-redux"
+// import { addRequests } from "../utils/requestSlice"
+// import { useEffect } from "react"
+
+// const Requests = () => {
+
+//     const dispatch = useDispatch()
+
+//     const requests = useSelector((store)=> store.requests)
+//     const fetchRequests = async ()=> {
+//         try {
+//             const res = await axios.get(BASE_URL +"/user/requests/received", {withCredentials: true})
+//             dispatch(addRequests(res.data.data))
+//         } catch (err) {
+//             console.log(err)
+//         }
+//     }
+
+//     useEffect(()=>{
+//         fetchRequests()
+//     },[])
+
+//     if (!requests) return
+
+//     if(requests.length ===0) return <h1>No Connections found</h1>
+//   return (
+
+//     <div>
+//         <div className='flex justify-center my-10'>
+//             <h1 className='font-bold text-3xl'>Requests</h1> 
+//         </div>
+
+//         {requests.map((request)=> {
+//           const {_id, firstName, lastName, photoUrl, age, gender, about } = request
+
+//         return (
+//           <div key={_id} className="flex justify-center">
+//           <div className="card bg-base-300 w-96 shadow-xl">
+//     <figure>
+//       <img
+//         src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+//         alt="Shoes" />
+//     </figure>
+//     <div className="card-body">
+//       <div className="card-actions justify-end">
+//       <button className="btn btn-secondary">Reject</button>
+//       <button className="btn btn-primary">Accept</button>
+//       </div>
+//     </div>
+//   </div>
+//           </div>
+//         )  
+//         })}
+
+ 
+//     </div>
+//   )
+// }
+
+// export default Requests
+
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addRequests } from "../utils/requestSlice";
+import { useEffect, useState } from "react";
+
+const Requests = () => {
+    const dispatch = useDispatch();
+    const requests = useSelector((store) => store.requests);
+    const [error, setError] = useState(null);
+
+    const fetchRequests = async () => {
+        try {
+            const res = await axios.get(BASE_URL + "/user/requests/received", { withCredentials: true });
+            console.log(res.data.data)
+            dispatch(addRequests(res.data.data));
+        } catch (err) {
+            setError("Failed to fetch requests. Please try again later.");
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchRequests();
+    }, []);
+
+    if (error) return <h1>{error}</h1>;
+    if (!requests) return <h1>Loading...</h1>;
+    if (requests.length === 0) return <h1>No Connections Found</h1>;
+
+    return (
+        <div>
+            <div className="flex justify-center my-10">
+                <h1 className="font-bold text-3xl">Requests</h1>
+            </div>
+            {requests.map((request) => {
+                const { _id, firstName, lastName, photoUrl, age, gender, about } = request.fromUserId;
+
+                return (
+                    <div key={_id} className="flex justify-center">
+                        <div className="card bg-base-300 w-96 shadow-xl">
+                            <figure>
+                                <img
+                                    src={photoUrl || "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"}
+                                    alt={`${firstName} ${lastName}`}
+                                />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{firstName} {lastName}</h2>
+                                <p>{about || "No information provided"}</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-secondary">Reject</button>
+                                    <button className="btn btn-primary">Accept</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default Requests;
